9627ed864e58b051c442a819db3b2177dc388c1e
Ellis Adigvom <ellisadigvom@expresspaygh.com>
2020-06-03 12:28:22 +0000

    follow psr-4

--- src/RequestFilter.php
+++ src/RequestFilter.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace expresspay\request_filter;
+
 /**
  * Filter incoming HTTP requests
  *
@@ -244,28 +246,6 @@ class RequestFilter
       }
     }
     return 0;
-  }
-}
-
-/**
- * A class to store functions we might want to run on incoming data before
- * performing filtering.
- */
-class FilterRules
-{
-  public static function check(string $func = null): bool
-  {
-    return (!is_null($func)) ? method_exists(__CLASS__, $func) : false;
-  }
-
-  public static function clean_string(string $string): string
-  {
-    return preg_replace("/[^A-Za-z0-9-_., ]/", "", $string);
-  }
-
-  public static function clean_tags(string $string): string
-  {
-    return strip_tags($string);
   }
 }
 
